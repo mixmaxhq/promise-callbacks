b32e90e7c4eef5c7c47e351ed6b8c95680d33b0e
Eli Skeggs <skeggse@users.noreply.github.com>
2017-06-30 10:09:53 -0700

    Update static.js

--- src/static.js
+++ src/static.js
@@ -30,7 +30,7 @@ function withTimeout(promise, delay, message='Operation timed out.') {
   const timeoutPromise = new Promise((resolve, reject) => {
     // Instantiate the error here to capture a more useful stack trace.
     const error = message instanceof Error ? message : new Error(message);
-    timeout = setTimeout(() => reject(error), delay);
+    timeout = setTimeout(reject, delay, error);
   });
   return Promise.race([promise, timeoutPromise]).then((value) => {
     clearTimeout(timeout);
